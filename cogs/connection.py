83c2c19d6322abffed00e32a4cc4d0f3962bed3a
Sergio Polanco <polancosergio8@gmail.com>
2023-04-08 19:33:15 -0400

    remove main2.py, a bit of code clenaup, include more events

--- cogs/connection.py
+++ cogs/connection.py
@@ -1,4 +1,6 @@
-from discord.ext.commands import Cog, Bot, command
+from discord.utils import find
+from discord.ext.commands import Cog, Bot, command, Context
+from discord import Guild, Message
 
 class DiscordClient(Cog):
     def __init__(self, bot: Bot) -> None:
@@ -10,12 +12,22 @@ class DiscordClient(Cog):
         print(f'{self.bot.user} succesfully logged in!')
     
     @Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message: Message) -> None:
         print(f'Message from {message.author}: {message.content}')
 
+    @Cog.listener()
+    async def on_guild_join(self, guild: Guild):
+        general = find(lambda x: x.name == 'general',  guild.text_channels)
+        if general and general.permissions_for(guild.me).send_messages:
+            await general.send('Hello next Yagsons from {}!'.format(guild.name))
+        
+        members = guild.members
+        for member in members:
+            print(member)
+
     @command()
-    async def ping(self, ctx):
+    async def ping(self, ctx: Context) -> None:
         await ctx.send('pong!')
 
-async def setup(bot: Bot):
+async def setup(bot: Bot) -> None:
     await bot.add_cog(DiscordClient(bot))
--- main.py
+++ main.py
@@ -11,17 +11,18 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 bot = Bot(command_prefix="!", intents=intents)
 
-async def load_extensions():
+async def load_extensions(bot_instance: Bot) -> None:
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
-            await bot.load_extension(f"cogs.{filename[:-3]}")
+            await bot_instance.load_extension(f"cogs.{filename[:-3]}")
 
-async def main():
-    async with bot:
-        await load_extensions()
-        await bot.start(TOKEN)
+async def main(bot_instance: Bot) -> None:
+    async with bot_instance:
+        await load_extensions(bot_instance)
+        await bot_instance.start(TOKEN)
 
-asyncio.run(main())
+asyncio.run(main(bot))
